7d849331f3bbb7da104f59d90f8ce124c462c48c
Gary Zhang <gzhang@primeobjects.com>
2022-05-20 17:09:35 -0700

    upgrade libs

--- run-upgrade-publish.sh
+++ run-upgrade-publish.sh
@@ -1,7 +1,3 @@
 set -e
 sh run-upgrade.sh
-sh run-tsc.sh
-sh run-test.sh
-
-npm version patch --no-git-tag-version
-npm publish
+sh run-publish.sh
---
+++ run-upgrade.bat
@@ -0,0 +1,3 @@
+call yarn upgrade douhub-helper-util --latest
+call yarn upgrade douhub-helper-service --latest
+call yarn upgrade douhub-helper-data --latest
